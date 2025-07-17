9e2fbfa7227101edd06bda8da4d0127df1b9c7c5
DHARSHAN KUMAR S <115333932+Dharshan1354@users.noreply.github.com>
2025-07-17 12:05:50 +0530

    Time: 9 ms (90.81%), Space: 45.1 MB (48.83%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,31 +1,29 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
-        int plen = p.length();
-        int slen = s.length();
-        if(plen > slen)
-            return list;
-        String temp = s.substring(0,plen);
-        if(isAnagram(temp,p))
-            list.add(0);
+        int slen = s.length(), plen = p.length();
+        if (plen > slen) return list;
 
-        for(int i=1; i<=slen - plen; i++){
-            temp = s.substring(i,i+plen);
-            if(isAnagram(temp,p))
-                list.add(i);
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        // Count frequencies for p and first window of s
+        for (int i = 0; i < plen; i++) {
+            pCount[p.charAt(i) - 'a']++;
+            sCount[s.charAt(i) - 'a']++;
         }
+
+        if (Arrays.equals(pCount, sCount)) list.add(0);
+
+        // Slide the window
+        for (int i = plen; i < slen; i++) {
+            sCount[s.charAt(i) - 'a']++; // add new char
+            sCount[s.charAt(i - plen) - 'a']--; // remove old char
+
+            if (Arrays.equals(pCount, sCount))
+                list.add(i - plen + 1);
+        }
+
         return list;
-    }
-    public boolean isAnagram(String s1, String s2){
-        int[] count = new int[26];
-        for(int i=0; i<s1.length(); i++){
-            count[s1.charAt(i) - 'a']++;
-            count[s2.charAt(i) - 'a']--;
-        }
-        for(int i=0; i<26; i++){
-            if(count[i] != 0)
-                return false;
-        }
-        return true;
     }
 }
