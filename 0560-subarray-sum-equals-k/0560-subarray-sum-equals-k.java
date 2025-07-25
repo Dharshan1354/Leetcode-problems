2d138a3d4a22ae9f3743353b5f1b16e0bd28f2eb
DHARSHAN KUMAR S <115333932+Dharshan1354@users.noreply.github.com>
2025-07-25 12:00:17 +0530

    Time: 23 ms (92.7%), Space: 46.4 MB (65.55%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,23 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum = 0;
-        int count = 0;
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        // Map to store how many times a prefix sum has occurred
+        HashMap<Integer, Integer> prefixSumFreq = new HashMap<>();
+        prefixSumFreq.put(0, 1); // Base case: one way to get sum = 0
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        int currentSum = 0; // Tracks the prefix sum at each step
+        int totalCount = 0; // Stores the total count of subarrays that sum to k
 
-            if (sum == k) {
-                count++;
+        for (int num : nums) {
+            currentSum += num; // Update the running prefix sum
+
+            // Check if there's a previous prefix sum that would make a subarray sum to k
+            int required = currentSum - k;
+
+            if (prefixSumFreq.containsKey(required)) {
+                totalCount += prefixSumFreq.get(required);
             }
 
-            if (hm.containsKey(sum - k)) {
-                count += hm.get(sum - k);
-            }
-
-            hm.put(sum, hm.getOrDefault(sum, 0) + 1);
+            // Update the frequency of the current prefix sum
+            prefixSumFreq.put(currentSum, prefixSumFreq.getOrDefault(currentSum, 0) + 1);
         }
 
-        return count;
+        return totalCount;
     }
 }
