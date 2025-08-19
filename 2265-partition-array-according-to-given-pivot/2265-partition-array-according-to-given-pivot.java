6119f0986e24f25c0017da65bc1816ac503739dc
DHARSHAN KUMAR S <115333932+Dharshan1354@users.noreply.github.com>
2025-08-20 00:01:04 +0530

    Time: 5 ms (76.18%), Space: 68.4 MB (14.95%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,38 +1,24 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int pivot_count = 0;
-        int smaller_pivot_count = 0;
+        int n = nums.length;
+        int[] res = new int[n];
+        int index = 0;
 
-        for(int i=0; i < nums.length; i++){
-            if(nums[i] == pivot)
-                pivot_count++;
-            else if(nums[i] < pivot)
-                smaller_pivot_count++;
-            else
-                continue;
-        }
-        int large_index = smaller_pivot_count + pivot_count ;
-
-        int[] res = new int[nums.length];
-        while(pivot_count > 0){
-            res[smaller_pivot_count] = pivot;
-            smaller_pivot_count++;  
-            pivot_count--;
+        // 1. Add elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) res[index++] = num;
         }
 
-        int smaller_index = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < pivot){
-                res[smaller_index] = nums[i];
-                smaller_index++;
-            }
-            else if(nums[i] > pivot){
-                res[large_index] = nums[i];
-                large_index++;
-            }
-            else
-                continue;
+        // 2. Add elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) res[index++] = num;
         }
+
+        // 3. Add elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) res[index++] = num;
+        }
+
         return res;
     }
 }
